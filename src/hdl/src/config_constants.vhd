68a7a40cb7099affe9a354a237b176054ca37700
Marc Rothmann <mrothmann@uni-osnabrueck.de>
2021-03-05 03:41:02 +0100

    Further optimized Q-Learning update.

--- src/hdl/src/config_constants.vhd
+++ src/hdl/src/config_constants.vhd
@@ -5,9 +5,9 @@ use IEEE.MATH_REAL.ALL;
 
 package config_constants is
     constant config_policy_random : integer := 1;
-    constant config_action_rams : integer := 1;
-    constant config_sarsa : integer := 1;
-    constant config_state_width : integer := 16;
-    constant config_action_width : integer := 3;
+    constant config_action_rams : integer := 0;
+    constant config_sarsa : integer := 0;
+    constant config_state_width : integer := 18;
+    constant config_action_width : integer := 2;
     constant config_reward_width : integer := 16;
 end config_constants;
